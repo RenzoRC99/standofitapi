4e80660fdbcd0bc39aacf68ced4d67ae81a3551a
RenzoRC99 <renzoromero99@hotmail.com>
2025-11-24 21:23:59 +0100

    first commit

---
+++ src/main/kotlin/com/standofit/standofitapi/StandofitapiApplication.kt
@@ -0,0 +1,11 @@
+package com.standofit.standofitapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StandofitapiApplication
+
+fun main(args: Array<String>) {
+    runApplication<StandofitapiApplication>(*args)
+}
