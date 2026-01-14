bb1b84bbdaa1d8a0e8bb3d4ff6a7991e99920294
Renzo <renzoromero99@hotmail.com>
2026-01-14 23:45:38 +0100

    Domain:  Refactor in WorkoutTplErrors.kt

--- src/main/kotlin/com/standofit/standofitapi/workouttpl/domain/exception/WorkoutTplErrors.kt
+++ src/main/kotlin/com/standofit/standofitapi/workouttpl/domain/exception/WorkoutTplErrors.kt
@@ -8,10 +8,8 @@ enum class WorkoutTplErrors(
 ) : ErrorInfo {
     DAY_ALREADY_EXISTS("WTPL_001", "Day already exists in this template"),
     DAY_NOT_FOUND("WTPL_002", "Day not found in this template"),
-    EXERCISE_ALREADY_EXISTS("WTPL_003", "Exercise already exists in this day"),
-    EXERCISE_NOT_FOUND("WTPL_004", "Exercise not found in this day"),
-    CANNOT_PUBLISH_EMPTY("WTPL_005", "Cannot publish a template without days"),
-    INVALID_STATUS_FOR_PUBLISH("WTPL_006", "Only draft templates can be published"),
-    ALREADY_ARCHIVED("WTPL_007", "Template is already archived"),
-    INVALID_STATUS_FOR_MODIFY("WTPL_008", "Only draft templates can be modified")
+    CANNOT_PUBLISH_EMPTY("WTPL_003", "Cannot publish a template without days"),
+    INVALID_STATUS_FOR_PUBLISH("WTPL_004", "Only draft templates can be published"),
+    ALREADY_ARCHIVED("WTPL_005", "Template is already archived"),
+    INVALID_STATUS_FOR_MODIFY("WTPL_006", "Only draft templates can be modified")
 }
